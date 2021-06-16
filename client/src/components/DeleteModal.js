56ee01e37813f583adbdca0351d4b606e4a60c21
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-06-16 17:21:09 -0600

    styled delete modal

--- client/src/components/DeleteModal.js
+++ client/src/components/DeleteModal.js
@@ -11,7 +11,7 @@ const DeleteModal = (props) => {
     }
 
     return(
-        <div>
+        <div className="delete-modal">
             <h1>Are you sure you want to delete?</h1>
             <button onClick={handleContinue}>Continue</button>
             <button onClick={handleCancel}>Cancel</button>
